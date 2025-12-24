a6dee5e404d6e4ebdb6e1f2216f0591f45c83e99
lifaet <lifaethossain@gmail.com>
2025-12-24 11:00:45 +0600

    .

---
+++ example.config.py
@@ -0,0 +1,15 @@
+# Rename this file to config.py and set your parameters
+
+# Device connection settings
+DEVICE_IP = '192.168.1.102'
+PORT = 4370
+POLL_INTERVAL = 5  # seconds
+DB_HOST = '127.0.0.1'
+DB_USER = 'root'
+DB_PASS = 'root'
+DB_NAME = 'attendance_db'
+TABLE_NAME = 'attendances'
+
+# For API server
+# API_TOKEN = "test12345"
+# API_PORT = 8080
