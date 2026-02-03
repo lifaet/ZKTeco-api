c2a29a5215d2762d3e5009a10bc892e2d968fa3a
lifaet <lifaethossain@gmail.com>
2026-02-03 10:18:52 +0600

    .

--- dashboard/resources/views/dashboard.blade.php
+++ dashboard/resources/views/dashboard.blade.php
@@ -354,7 +354,9 @@ footer .maintenance a:hover {
     <a href="#" data-type="monthly"><i class="bi bi-calendar-month"></i> Monthly</a>
     <a href="#" data-type="user"><i class="bi bi-person-circle"></i> User-wise</a>
     <a href="#" data-type="directory"><i class="bi bi-people"></i> User Directory</a>
+    @if(env('ATTENDANCE2_ENABLED'))
     <a href="/attendance2" target="_blank"><i class="bi bi-clock"></i> Attendance2</a>
+    @endif
     <hr>
     <a href="#" id="logoutBtn" class="logout-btn"><i class="bi bi-box-arrow-right"></i> Logout</a>
 </div>
